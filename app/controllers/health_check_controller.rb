e1ef6545ff0e17841ea4bfe3726711bba1c7510e
Henrique Barbosa <henrikleod@gmail.com>
2022-07-07 14:34:13 -0300

    feature: json response

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -1,5 +1,17 @@
 class HealthCheckController < ApplicationController
-  def show
+  before_action :fetch_health_status
+
+  def show;end
+
+  def health
+    respond_to do |format|
+      format.json { render json: @report }
+    end
+  end
+  
+  private
+
+  def fetch_health_status
     @report = HealthCheckStatus.get
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get '/' => "health_check#show"
+  get '/health' => "health_check#health"
 end
