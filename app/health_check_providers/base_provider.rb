0ce87970ef2b15683b960352c1fe6128b9185faa
Henrique Barbosa <henrikleod@gmail.com>
2022-07-07 13:05:55 -0300

    feature: basic provider

---
+++ app/health_check_providers/base_provider.rb
@@ -0,0 +1,45 @@
+class BaseProvider
+  APPLICATION_STATUSES = {
+    normal: 'Normal',
+    unstable: 'Unstable',
+    down: 'Down'
+  }.freeze
+
+  ACCEPTABLE_TIMEOUT = 20
+
+  def self.get_endpoint_status(app:, url:)
+    @response = RestClient::Request.execute(
+      method: :get,
+      url:,
+      timeout: ACCEPTABLE_TIMEOUT
+    )
+
+    OpenStruct.new({
+      app:,
+      url:,
+      time: get_response_time,
+      status: get_status
+    })
+  end
+
+  private
+
+  def self.get_response_time
+    start_time = @response.start_time.strftime('%s%3N').to_i # time to milliseconds
+    end_time   = @response.end_time.strftime('%s%3N').to_i
+
+    end_time - start_time
+  end
+
+  def self.get_status
+    status = :normal
+
+    if @response.code >= 500
+      status = :down
+    elsif get_response_time > ACCEPTABLE_TIMEOUT*1000
+      status = :unstable
+    end
+
+    APPLICATION_STATUSES[status]
+  end
+end
