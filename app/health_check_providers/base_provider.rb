0613379c58b9ea348b1955aab70a58178de9ad0c
Henrique Barbosa <henrikleod@gmail.com>
2022-07-07 14:24:10 -0300

    feature: improving base provider

--- app/health_check_providers/base_provider.rb
+++ app/health_check_providers/base_provider.rb
@@ -8,18 +8,28 @@ class BaseProvider
   ACCEPTABLE_TIMEOUT = 20
 
   def self.get_endpoint_status(app:, url:)
-    @response = RestClient::Request.execute(
-      method: :get,
-      url:,
-      timeout: ACCEPTABLE_TIMEOUT
-    )
+    begin
+      @response = RestClient::Request.execute(
+        method: :get,
+        url:,
+        timeout: ACCEPTABLE_TIMEOUT
+      )
 
-    OpenStruct.new({
-      app:,
-      url:,
-      time: get_response_time,
-      status: get_status
-    })
+      ## FIXME: add an rescue here
+      OpenStruct.new({
+        app:,
+        url:,
+        time: get_response_time,
+        status: get_status
+      })
+    rescue => e
+      OpenStruct.new({
+        app:,
+        url:,
+        time: 0,
+        status: APPLICATION_STATUSES[:down]
+      })
+    end
   end
 
   private
